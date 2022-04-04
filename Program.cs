77b183f36efe989bf96d8a98a79f67d4065913e6
BrookeAL0012 <bbcecil17@gmail.com>
2022-04-04 13:08:25 -0500

    initial commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace VariablesProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Variablle Declarations and Initializations
+
+            string dogName = "Clementine";
+
+            int dogAge = 16;
+
+            bool isAlive = true;
+               
+            char dogChar = 'C';
+
+            double dogWeight = 45.55;
+
+            decimal dogTreats = 4.5m;
+
+            Console.WriteLine($"My dog's name is {dogName}, She is {dogAge} years old', She weighs {dogWeight} pounds and she gets {dogTreats} treats per day, She is alive {isAlive} and is represented by {dogChar}");
+
+
+
+
+
+
         }
     }
 }
